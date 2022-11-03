e308ac1349e383b2c59149480732b91661eb7629
kriziu <brunodzi07@gmail.com>
2022-11-03 19:12:47 +0100

    Add opacity down on scroll

--- modules/projects/components/Header.tsx
+++ modules/projects/components/Header.tsx
@@ -4,15 +4,24 @@ import { motion } from 'framer-motion';
 
 import ScrollOpacity from '@/common/components/ScrollOpacity';
 import { useScrollY } from '@/common/hooks/useScrollY';
+import { useWindowSize } from '@/common/hooks/useWindowSize';
 import { useMouseVariant } from '@/modules/customMouse';
 
 const Header = () => {
   const scrollY = useScrollY();
   const { setMouseVariant } = useMouseVariant();
 
+  const { height } = useWindowSize();
+
   const [startScroll, setStartScroll] = useState(0);
 
   const scale = Math.max((scrollY - startScroll) / 5000 + 0.2);
+
+  let opacity = 1;
+
+  if (scrollY > startScroll + height * 1.5) {
+    opacity = 0.9 - (scrollY - (startScroll + height * 1.5)) / 400;
+  }
 
   return (
     <div className="absolute z-50 flex h-full w-full flex-col items-center justify-center px-10">
@@ -21,7 +30,7 @@ const Header = () => {
           className="w-max text-center"
           onMouseEnter={setMouseVariant.text}
           onMouseLeave={setMouseVariant.default}
-          style={{ scale, fontSize: 'max(10vw, 15vh)' }}
+          style={{ scale, fontSize: 'max(10vw, 15vh)', opacity }}
         >
           and more.
         </motion.p>
