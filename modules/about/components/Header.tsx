cdda044396a89729eab82bea1e2124a56f5020c7
Bruno Dzięcielski <brunodzi07@gmail.com>
2022-11-03 10:29:50 +0100

    Cleanups

--- modules/about/components/Header.tsx
+++ modules/about/components/Header.tsx
@@ -1,22 +1,10 @@
 import { motion } from 'framer-motion';
 
-import { useScrollY } from '@/common/hooks/useScrollY';
-import { useWindowSize } from '@/common/hooks/useWindowSize';
-import { useMouseVariant } from '@/modules/customMouse';
 import ScrollOpacity from '@/common/components/ScrollOpacity';
+import { useMouseVariant } from '@/modules/customMouse';
 
 const AboutHeader = () => {
   const { setMouseVariant } = useMouseVariant();
-  const scrolled = useScrollY();
-
-  const { width } = useWindowSize();
-
-  const mobile = width < 768;
-
-  // const scale = Math.max(
-  //   0,
-  //   (mobile ? 1.3 : 1) - scrolled / (mobile ? 1400 : 3500)
-  // );
 
   return (
     <div className="flex h-full w-full items-center justify-center">
