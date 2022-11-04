8994a22e1d40da4361516a84683534321162a0f0
kriziu <brunodzi07@gmail.com>
2022-11-05 00:57:02 +0100

    Add navigation animation

--- modules/hero/index.tsx
+++ modules/hero/index.tsx
@@ -1,6 +1,7 @@
 import { RefObject } from 'react';
 
 import { IParallax } from '@react-spring/parallax';
+import { motion } from 'framer-motion';
 
 import { useWindowSize } from '@/common/hooks/useWindowSize';
 
@@ -14,7 +15,12 @@ const Hero = ({ parallaxRef }: { parallaxRef: RefObject<IParallax> }) => {
 
   return (
     <div className="relative flex h-full w-full flex-col items-center justify-center overflow-hidden">
-      <div className="absolute top-8 flex items-center gap-5 font-sans text-lg md:gap-10">
+      <motion.div
+        className="absolute top-8 flex items-center gap-5 font-sans text-lg md:gap-10"
+        initial={{ y: -20, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.3, delay: 0.65 }}
+      >
         <div className="h-px bg-zinc-600 sm:w-36 md:w-48 lg:w-72" />
         <button
           onClick={() => parallaxRef.current?.scrollTo(width < 768 ? 8.4 : 9)}
@@ -37,7 +43,7 @@ const Hero = ({ parallaxRef }: { parallaxRef: RefObject<IParallax> }) => {
           Contact
         </button>
         <div className="h-px bg-zinc-600 sm:w-36 md:w-48 lg:w-72" />
-      </div>
+      </motion.div>
 
       <div className="flex w-max flex-1 flex-col">
         <div className="flex h-[55%] items-end">
